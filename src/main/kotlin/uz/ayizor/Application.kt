018aac7afecf59e2fa80b7d8a1758e5c97ea6329
diyorbek57 <Diyorbek1727>
2022-10-15 02:19:41 +0500

    Project created

---
+++ src/main/kotlin/uz/ayizor/Application.kt
@@ -0,0 +1,15 @@
+package uz.ayizor
+
+import io.ktor.server.application.*
+import uz.ayizor.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureSecurity()
+    configureMonitoring()
+    configureSerialization()
+    configureRouting()
+}
